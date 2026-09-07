adef1bf46f9239122141d10d1d3bafe5d7473f83
Kosho969 <pablo.koch26@gmail.com>
2026-09-07 08:24:40 -0600

    comitting the initial setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Attendance app frontend is running')
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+  </main>
+</template>
